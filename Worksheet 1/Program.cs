a464e8ddda4c7e53140ce84310d300f9198606ef
Damian Polakovs <S00236491@atu.ie>
2023-01-30 09:20:53 +0000

    fixed

--- Worksheet 1/Program.cs
+++ Worksheet 1/Program.cs
@@ -2,9 +2,79 @@
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             * 
+             * Name: Damian Polakovs
+             * Date: 25/1/2023
+             * Desc: Q1
+             * 
+             */
+
+            PrintIntegersFor();
+            PrintIntegersWhile();
+            PrintIntegersDo();
+            PrintExcludingFor();
+            PrintDescendingFor();   
+
         }
+
+        static private void PrintIntegersFor()
+        {
+            for (int i = 40; i <= 60; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static private void PrintIntegersWhile()
+        {
+            int x = 0;
+           
+            while (x >= 40 && x <= 60)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+        }
+
+        static private void PrintIntegersDo()
+        {
+            int y = 0;
+            int counter = 0;
+            do
+            {
+
+
+                counter = counter + y;
+                Console.WriteLine(y);
+            }
+
+            while (y >= 40 && y <= 60);
+            
+        }
+
+        static private void PrintExcludingFor()
+        {
+            for (int n = 40 ;n <= 60 && (n != 46 && n != 48); n++)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        static private void PrintDescendingFor()
+        {
+            for (int z = 60; z>=40; z--)
+            {
+                Console.WriteLine(z);
+            }
+
+        }
+            
+            
+           
+            
     }
 }
