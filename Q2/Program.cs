402bd8369fd344262d4885d77f30d62df6ff9fd7
Damian Polakovs <S00236491@atu.ie>
2023-01-30 09:32:30 +0000

    Q2 started

---
+++ Q2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             * Name: Damian Polakovs
+             * Date: 30/1/2023
+             * Desc: Q2
+        }
+    }
+}
